75ff505a70874a377b886c7a3e3414700f274d1e
chartataglance <mariadass@msn.com>
2022-11-03 07:08:14 +0000

    dd

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
-$url = explode("/", $_SERVER['QUERY_STRING']);
+// (A) SETTINGS
+define("URL_BASE", "/");
+define("PATH_PAGES", __DIR__ . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR);
+ 
+// (B) PARSE URL PATH INTO AN ARRAY
+// e.g. http://site.com > [""]
+// e.g. http://site.com/foo > ["foo"]
+// e.g. http://site.com/foo/bar > ["foo", "bar"]
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+if (substr($path, 0, strlen(URL_BASE)) == URL_BASE) {
+  $path = substr($path, strlen(URL_BASE));
+}
+$path = explode("/", rtrim($path, "/\\"));
 
-echo"<pre>";
-print_r($url);
-echo"<pre>";
+// (C) LOAD REQUESTED PAGE ACCORDINGLY
+// (C1) URL PATH TO PHYSICAL FILE
+// e.g. http://site.com > index.html
+// e.g. http://site.com/foo > foo.html
+// e.g. http://site.com/foo/bar > foo-bar.html
+if (count($path)==1) { 
+  $file = $path[0]=="" ? "index.html" : $path[0] . ".html";
+} else {
+  $file = implode("-", $path) . ".html"; 
+}
 
-
-
-?>
+// (C2) LOAD PHYSICAL FILE
+if (file_exists(PATH_PAGES . $file)) { require PATH_PAGES . $file; }
+else {
+  http_response_code(404);
+  require PATH_PAGES . "404.html";
+}
